d39fb717ac24acd726089b5a79b957de507c29a8
spectre6000 <spectre6000@gmail.com>
2015-10-16 13:13:44 -0600

    Author attribute migration details

--- db/migrate/20151016191006_add_attributes_to_author.rb
+++ db/migrate/20151016191006_add_attributes_to_author.rb
@@ -1,4 +1,9 @@
 class AddAttributesToAuthor < ActiveRecord::Migration
   def change
+    change_table :authors do |t|
+      t.boolean     :admin, default: false
+      t.text        :bio
+      t.string      :location
+    end
   end
 end
