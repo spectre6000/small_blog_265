c7063fbc3d4479c272653e96fbe94ce73bf538a2
spectre6000 <spectre6000@gmail.com>
2016-04-28 15:39:08 -0600

    Add article body to model

--- db/migrate/20160428183730_add_title_and_body_to_articles.rb
+++ db/migrate/20160428183730_add_title_and_body_to_articles.rb
@@ -1,7 +1,6 @@
 class AddTitleAndBodyToArticles < ActiveRecord::Migration
   def change
     change_table :articles do |t|
-      t.string     :title
       t.text       :body
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426152250) do
+ActiveRecord::Schema.define(version: 20160428183730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160426152250) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "body"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
